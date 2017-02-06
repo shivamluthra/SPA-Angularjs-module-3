c4c4f889fcf1006d9ced916a5346bd326d2d7ad8
Shivam Luthra <sluthra145@gmail.com>
2017-02-06 13:16:32 +0530

    slight changes

--- app.js
+++ app.js
@@ -1,71 +1,89 @@
 (function(){
-    'use strict';
+'use strict';
 
-    angular.module('NarrowItDownApp',[])
-    .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService)
-    .constant('ApiBasePath',"https://davids-restaurant.herokuapp.com")
-    .directive('listFoundItems', ListFoundItems);
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+.directive('listFoundItems', ListFoundItems);
 
-    function ListFoundItems() {
-      var ddo = {
-        templateUrl: 'foundItems.html',
-        scope: {
-          items: '<',
-          menu: '=foundItems',
-          badRemove: '=',
-          onRemove: '&'
-        },
-        // controller: NarrowItDownController,
-        // controllerAs: 'menu',
-        // bindToController: true
-      };
-      return ddo;
-    }
 
-    NarrowItDownController.$inject = ['MenuSearchService'];
-    function NarrowItDownController(MenuSearchService){
-        var menu = this;
-        //menu.itemName = "";
-        menu.searchItems = function () {
-          console.log("Searching for "+menu.itemName);
-          MenuSearchService.getMatchedMenuItems(menu.itemName)
+
+function ListFoundItems() {
+  var ddo = {
+    templateUrl: 'list.html',
+    scope: {
+      items: '<',
+      menu: '=foundItems',
+      badRemove: '=',
+      onRemove: '&'
+    },
+    // controller: ListFoundItemsController,
+    //  controllerAs: 'menu',
+    //      transclude: true
+//link: listFoundItemsLink,
+//transclude: true
+};
+
+
+  return ddo;
+}
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService){
+  var menu = this;
+
+  menu.getCorrectItems = function () {
+      MenuSearchService.getMatchedMenuItems(menu.itemName)
       .then(function (response){
         menu.found = response;
       })
       .catch(function (error) {
         console.log("Something went terribly wrong.");
       });
-    };
-        menu.removeItem = function (itemIndex) {
-          console.log("Removing...");
-          menu.found.splice(itemIndex,1);
-        };
-    }
+      if (menu.found.length<=0) {
+        menu.errormessage = "Nothing found";
+      }
+  };
 
-    MenuSearchService.$inject = ['$http','ApiBasePath'];
-    function MenuSearchService($http,ApiBasePath){
-        var service = this;
+  menu.removeItem = function (itemIndex) {
+    menu.found.splice(itemIndex, 1);
+    console.log('remove');
+  };
 
-        service.getMatchedMenuItems = function(itemName){
+}
 
-            return $http({
-                method : "GET",
-                url : (ApiBasePath + "/menu_items.json")
-            }).then(function success(result) {
-              var founditems = [];
-              for (var item in result.data.menu_items){
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+
+
+  service.getMatchedMenuItems = function (itemName) {
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    }).then(function (result) {
+        var  foundItems = []
+      for (var item in result.data.menu_items){
          if (result.data.menu_items[item].description.includes(itemName)){
            foundItems.push(result.data.menu_items[item])
            //console.log(result.data.menu_items[item]);
          }
-              }
-              return founditems;
-            },
-            function error(response) {
-              console.log("ERROR");
-            });
-          };
+      }
+        // process result and only keep items that match
+        // return processed items
+        return foundItems;
 
-        }
+    });
+  };
+
+}
+
+
+
+
 })();
